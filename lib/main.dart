083b05825ad21ec2865891fcdc2738376776f8a4
waffler <kevingbeyer@gmail.com>
2026-09-03 12:06:31 -0500

    YOOOOOOOOOOOOOOOOOOO

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,14 @@ class _SplashCreenState extends State<SplashScreen> {
       height: 100,
       decoration: BoxDecoration(
         // I understand RGB more than this silly color list
-        color: Color.fromARGB(255, 50, 50, 50),
+        //color: Color.fromARGB(255, 50, 50, 50),
+        image: DecorationImage(
+          image: NetworkImage(
+            // beautiful picture of colorado I found online
+            "https://www.colorado.com/_next/image?url=%2Fsites%2Fdefault%2Ffiles%2F2025-07%2F2023_1_1770%2520%25281%2529.jpg&w=2048&q=75",
+          ),
+          fit: BoxFit.cover,
+        ),
         border: Border.all(color: Colors.blueAccent, width: 10),
       ),
 
